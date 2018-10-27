73aa2b0ae9516a5e49a5c046b94d8377f9323b64
Jonas Höglund <firefly@firefly.nu>
2018-10-27 07:58:48 +0200

    Add infobox, button control, default to no selected station

--- src/components/TrainMap.js
+++ src/components/TrainMap.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Label, Segment } from 'semantic-ui-react';
 import { CircleMarker, Map, TileLayer, Polyline, Popup } from 'react-leaflet';
 import loadCSVData from '../utilities/loadCSVData';
 import distanceFromStop from '../services/routing/distanceFromStop';
@@ -13,28 +14,42 @@ const relevantStops = stops.filter(({ stopId }) => relevantStopIds.has(stopId));
 
 const colorMap = [
   '#00cc00', // 0
-  '#cc8800', // 1
+  '#ffaa00', // 1
   '#cc0000', // 2
-  '#cc00cc', // 3
+  '#0066ff', // 3
+  '#999999', // 4
 ];
+
+const ppDestinations = (n, withAre) => {
+  const nPretty = n === 0 ? 'No' : String(n);
+  const destinations = n === 1 ? 'destination' : 'destinations';
+  const are = withAre ? (n === 1 ? 'is' : 'are') : '';
+  return (
+    <span><strong>{nPretty}</strong> {destinations} {are}</span>
+  );
+}
 
 const Center = [61.500, 14.500];
 const Zoom = 6;
 
-const selectedStopId0 = '740000923'; // Högsby
 class TrainMap extends Component {
   state = {
-    selectedStopId: selectedStopId0,
+    selectedStopId: null,
+    selectedStopName: null,
+    steps: 1,
   }
 
-  handlePressStop = (stopId) => {
-    this.setState({ selectedStopId: stopId });
+  handlePressStop = (stopId, stopName) => {
+    this.setState({ selectedStopId: stopId, selectedStopName: stopName });
+  }
+
+  handleChooseSteps = (n) => {
+    this.setState({ steps: n });
   }
 
   renderStops = ({ toStop }) => {
     return relevantStops.map(({ stopId, stopLat, stopLon, stopName }) => {
       const dist = toStop[stopId];
-   // const color = colorMap[dist] || '#999'
       const color = '#999';
       const radius = dist === 0 ? 5 : 2;
       return (
@@ -44,18 +59,17 @@ class TrainMap extends Component {
           radius={radius}
           color={color}
         >
-          <Popup onOpen={() => this.handlePressStop(stopId)}>{ `${stopId} ${stopName}` }</Popup>
+          <Popup onOpen={() => this.handlePressStop(stopId, stopName)}>{ stopName }</Popup>
         </CircleMarker>
       );
     });
   }
 
   renderRoutesForStop = ({ toRoute }) => {
+    const { steps } = this.state;
     const routesToRender = Object.keys(toRoute)
+      .filter(n => toRoute[n] < steps)
       .sort((a, b) => toRoute[b] - toRoute[a]);
-    console.warn('render first', routesToRender[0], toRoute[routesToRender[0]]);
-    const last = routesToRender.slice(-1)[0];
-    console.warn('render last', last, toRoute[last]);
     return routesToRender.map(routeId => (
         <Polyline
           key={routeId}
@@ -66,21 +80,121 @@ class TrainMap extends Component {
       ));
     }
 
-  render() {
-    const { selectedStopId } = this.state;
-    console.time('distanceFromStop');
-    const distance = distanceFromStop(selectedStopId);
-    console.timeEnd('distanceFromStop');
+  renderStepButtons = () => {
+    const { steps } = this.state;
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 14,
+          left: 14,
+          zIndex: 1000000,
+        }}
+      >
+        <Label basic color="red" pointing='right' style={{ marginRight: 14 }}>
+          # Trains away
+        </Label>
+        <Button.Group widths={1}>
+          {
+            [1, 2, 3, 4, 5].map(n => (
+              <Button
+                key={n}
+                active={n === steps}
+                style={{
+                  color: n === steps ? '#eee' : colorMap[n - 1],
+                  backgroundColor: n === steps ? colorMap[n - 1] : '#eee',
+                }}
+                onClick={() => this.handleChooseSteps(n)}
+              >
+                {n}
+              </Button>
+            ))
+          }
+        </Button.Group>
+      </div>
+    );
+  }
+
+  renderInfoBox = ({ toStop }) => {
+    const { selectedStopName } = this.state;
+
+    const distanceFreq = {};
+    let distanceFreqSum = 0;
+    Object.values(toStop).forEach((v) => {
+      distanceFreq[v] = (distanceFreq[v] || 0) + 1;
+      distanceFreqSum += 1;
+    });
 
     return (
-      <Map style={{ height: '100%' }} center={Center} zoom={Zoom}>
-        <TileLayer
-          attribution="&amp;copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        { this.renderRoutesForStop(distance) }
-        { this.renderStops(distance) }
-      </Map>
+      <div
+        style={{
+          position: 'absolute',
+          top: 14,
+          right: 14,
+          width: 400,
+          zIndex: 1000000,
+        }}
+      >
+        <Segment>
+          {
+            selectedStopName == null &&
+            <div>
+              <p>
+                <strong>Welcome!</strong> Press a station marker to show all
+                routes passing through this stop.
+              </p>
+              <p>
+                Choose in the bottom left to select how many trains away you
+                want to show routes for.
+              </p>
+              <p>
+                <strong>Example:</strong> Three trains away means you have to
+                make two changes to reach this destination, minimum.
+              </p>
+            </div>
+          }
+          {
+            selectedStopName != null &&
+            <div>
+              <p>
+                From <strong>{selectedStopName}</strong> you can reach <strong>{distanceFreqSum}</strong> destinations.
+              </p>
+              <p>
+                Out of these,
+              </p>
+              <ul>
+                <li>{ppDestinations(distanceFreq[1] || 0)} can be reached <strong style={{ color: colorMap[0] }}>directly</strong></li>
+                <li>{ppDestinations(distanceFreq[2] || 0, true)} are <strong style={{ color: colorMap[1] }}>two</strong> trains away</li>
+                <li>{ppDestinations(distanceFreq[3] || 0, true)} are <strong style={{ color: colorMap[2] }}>three</strong> trains away</li>
+                <li>{ppDestinations(distanceFreq[4] || 0, true)} are <strong style={{ color: colorMap[3] }}>four</strong> trains away</li>
+                <li>{ppDestinations(distanceFreq[5] || 0, true)} are <strong style={{ color: colorMap[4] }}>five</strong> trains away</li>
+              </ul>
+            </div>
+          }
+        </Segment>
+      </div>
+    );
+  }
+
+  render() {
+    const { selectedStopId } = this.state;
+    const distance = selectedStopId == null
+      ? { toStop: {}, toRoute: {} }
+      : distanceFromStop(selectedStopId);
+
+    return (
+      <div style={{ height: '100%' }}>
+        { this.renderStepButtons() }
+        { this.renderInfoBox(distance) }
+        <Map style={{ height: '100%' }} center={Center} zoom={Zoom}>
+          <TileLayer
+            attribution="&amp;copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          { this.renderRoutesForStop(distance) }
+          { this.renderStops(distance) }
+        </Map>
+      </div>
     )
   }
 }
