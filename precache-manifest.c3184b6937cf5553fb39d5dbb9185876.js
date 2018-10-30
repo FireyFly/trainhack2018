5c02eec7756d752396550647cc0843f50237d216
Jonas Höglund <firefly@firefly.nu>
2018-10-30 19:26:00 +0100

    Updates

---
+++ precache-manifest.c3184b6937cf5553fb39d5dbb9185876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3612ce7bbf38c0d56751",
+    "url": "/trainhack2018/static/js/runtime~main.3612ce7b.js"
+  },
+  {
+    "revision": "023427f619deccceec24",
+    "url": "/trainhack2018/static/js/main.023427f6.chunk.js"
+  },
+  {
+    "revision": "ce50e6243729107556f3",
+    "url": "/trainhack2018/static/js/1.ce50e624.chunk.js"
+  },
+  {
+    "revision": "023427f619deccceec24",
+    "url": "/trainhack2018/static/css/main.f943e4a4.chunk.css"
+  },
+  {
+    "revision": "ab36db6cb585e1d83e20a537d9f4edf4",
+    "url": "/trainhack2018/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65281c32342efb9aaa2daf3e55f78af8.js"
+  "/trainhack2018/precache-manifest.c3184b6937cf5553fb39d5dbb9185876.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/trainhack2018/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
